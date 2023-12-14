eeebfd833d946a8fa309f281c6923f7abf6e6192
feraounabdelaziz <azizmahon10@gmail.com>
2023-12-14 14:40:43 +0100

    last update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'should-work',
+  appName: 'ez-attendance',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
